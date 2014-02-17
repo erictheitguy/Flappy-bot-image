2a069bb00cd2ce62ce87eb390a05f151efad441e
Eric <eric@kurtti.net>
2014-02-17 15:18:20 -0700

    Basic function for detecting main start screen function.

--- logomatch/fmhmgraphy.py
+++ logomatch/fmhmgraphy.py
@@ -1,1 +1,176 @@
 __author__ = 'Eric'
+import numpy as np
+import cv2
+from matplotlib import pyplot as plt
+
+MIN_MATCH_COUNT = 10
+cv2.namedWindow("preview")
+vc = cv2.VideoCapture(0)
+
+if vc.isOpened(): # try to get the first frame
+    rval, frame = vc.read()
+else:
+    rval = False
+
+img2 = cv2.imread('flappybird-logo.png',0) #loaded image as gray
+img1 = frame # trainImage
+gray_img = cv2.cvtColor(img1,cv2.COLOR_BGR2GRAY)
+# Initiate SIFT detector
+sift = cv2.SIFT()
+
+# find the keypoints and descriptors with SIFT
+kp1, des1 = sift.detectAndCompute(gray_img,None)
+kp2, des2 = sift.detectAndCompute(img2,None)
+
+FLANN_INDEX_KDTREE = 0
+index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
+search_params = dict(checks = 50)
+
+flann = cv2.FlannBasedMatcher(index_params, search_params)
+
+matches = flann.knnMatch(des1,des2,k=2)
+
+# store all the good matches as per Lowe's ratio test.
+mask = None
+M = None
+
+good = []
+for m,n in matches:
+    if m.distance < 0.7*n.distance:
+        good.append(m)
+if len(good)>MIN_MATCH_COUNT:
+    src_pts = np.float32([ kp1[m.queryIdx].pt for m in good ]).reshape(-1,1,2)
+    dst_pts = np.float32([ kp2[m.trainIdx].pt for m in good ]).reshape(-1,1,2)
+
+    M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC,5.0)
+
+    matchesMask = mask.ravel().tolist()
+
+    h,w = img1.shape
+    pts = np.float32([ [0,0],[0,h-1],[w-1,h-1],[w-1,0] ]).reshape(-1,1,2)
+    dst = cv2.perspectiveTransform(pts,M)
+
+    img2 = cv2.polylines(img2,[np.int32(dst)],True,255,3, cv2.LINE_AA)
+
+else:
+    print "Not enough matches are found - %d/%d" % (len(good),MIN_MATCH_COUNT)
+    matchesMask = None
+draw_params = dict(matchColor = (0,255,0), # draw matches in green color
+                   singlePointColor = None,
+                   matchesMask = matchesMask, # draw only inliers
+                   flags = 2)
+
+img3 = cv2.drawMatches(img1,kp1,img2,kp2,good,None,**draw_params)
+good_M = M
+good_mask = mask
+while rval:
+    cv2.imshow("preview", img3)
+    rval, frame = vc.read()
+    img1 = frame
+    gray_img = cv2.cvtColor(img1,cv2.COLOR_BGR2GRAY)
+    # Initiate SIFT detector
+    sift = cv2.SIFT()
+
+    # find the keypoints and descriptors with SIFT
+    kp1, des1 = sift.detectAndCompute(gray_img,None)
+    kp2, des2 = sift.detectAndCompute(img2,None)
+
+    FLANN_INDEX_KDTREE = 0
+    index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
+    search_params = dict(checks = 50)
+
+    flann = cv2.FlannBasedMatcher(index_params, search_params)
+
+    matches = flann.knnMatch(des1,des2,k=2)
+
+    # store all the good matches as per Lowe's ratio test.
+    good = []
+    for m,n in matches:
+        if m.distance < 0.7*n.distance:
+            good.append(m)
+    if len(good)>MIN_MATCH_COUNT:
+        src_pts = np.float32([ kp1[m.queryIdx].pt for m in good ]).reshape(-1,1,2)
+        dst_pts = np.float32([ kp2[m.trainIdx].pt for m in good ]).reshape(-1,1,2)
+
+        M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC,5.0)
+        #The follow is a workaround for a bug in the cv2.RANSAC method
+        if mask is None:
+            print "we going to crash"
+            M, mask = cv2.findHomography(src_pts, dst_pts,0,5.0)
+            # lets reuse a last good mask
+            #mask = good_mask
+            #M = good_M
+            matchesMask = mask.ravel().tolist()
+        else:
+            matchesMask = mask.ravel().tolist()
+            good_M = M
+            good_mask = mask
+        h,w = img2.shape
+        pts = np.float32([ [0,0],[0,h-1],[w-1,h-1],[w-1,0] ]).reshape(-1,1,2)
+        dst = cv2.perspectiveTransform(pts,M)
+
+        img1 = cv2.polylines(img1,[np.int32(dst)],True,255,3, cv2.LINE_AA)
+        print "Enough matches are found - %d/%d" % (len(good),MIN_MATCH_COUNT)
+        #we would then call the next routine to start the game
+        #now we have to look for the ready screen
+    else:
+        print "Not enough matches are found - %d/%d" % (len(good),MIN_MATCH_COUNT)
+        matchesMask = None
+    draw_params = dict(matchColor = (0,255,0), # draw matches in green color
+                       singlePointColor = None,
+                       matchesMask = matchesMask, # draw only inliers
+                       flags = 2)
+    img3 = cv2.drawMatches(img1,kp1,img2,kp2,good,None,**draw_params)
+    key = cv2.waitKey(20)
+    if key == 27: # exit on ESC
+        break
+cv2.destroyWindow("preview")
+
+
+
+
+#
+# # Initiate SIFT detector
+# sift = cv2.SIFT()
+#
+# # find the keypoints and descriptors with SIFT
+# kp1, des1 = sift.detectAndCompute(img1,None)
+# kp2, des2 = sift.detectAndCompute(img2,None)
+#
+# FLANN_INDEX_KDTREE = 0
+# index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
+# search_params = dict(checks = 50)
+#
+# flann = cv2.FlannBasedMatcher(index_params, search_params)
+#
+# matches = flann.knnMatch(des1,des2,k=2)
+#
+# # store all the good matches as per Lowe's ratio test.
+# good = []
+# for m,n in matches:
+#     if m.distance < 0.7*n.distance:
+#         good.append(m)
+# if len(good)>MIN_MATCH_COUNT:
+#     src_pts = np.float32([ kp1[m.queryIdx].pt for m in good ]).reshape(-1,1,2)
+#     dst_pts = np.float32([ kp2[m.trainIdx].pt for m in good ]).reshape(-1,1,2)
+#
+#     M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC,5.0)
+#     matchesMask = mask.ravel().tolist()
+#
+#     h,w = img1.shape
+#     pts = np.float32([ [0,0],[0,h-1],[w-1,h-1],[w-1,0] ]).reshape(-1,1,2)
+#     dst = cv2.perspectiveTransform(pts,M)
+#
+#     img2 = cv2.polylines(img2,[np.int32(dst)],True,255,3, cv2.LINE_AA)
+#
+# else:
+#     print "Not enough matches are found - %d/%d" % (len(good),MIN_MATCH_COUNT)
+#     matchesMask = None
+# draw_params = dict(matchColor = (0,255,0), # draw matches in green color
+#                    singlePointColor = None,
+#                    matchesMask = matchesMask, # draw only inliers
+#                    flags = 2)
+#
+# img3 = cv2.drawMatches(img1,kp1,img2,kp2,good,None,**draw_params)
+#
+# plt.imshow(img3, 'gray'),plt.show()
