ca095754f5e70830b68a0e9f1673e9102c240fb9
Eric <eric@kurtti.net>
2014-03-15 18:06:40 -0600

    min max bounding box

--- logomatch/fhgraphy.py
+++ logomatch/fhgraphy.py
@@ -54,18 +54,42 @@ if len(good)>MIN_MATCH_COUNT:
 
     #matches mask has flag for each src_pts if 1 draw if 0 not draw
     #use match mask
-    for xy_pts in src_pts:
 
-        #print xy_pts
-        x_pts = xy_pts[0,0]
-        y_pts = xy_pts[0,1]
-        dist = math.hypot(mx_pts - x_pts, my_pts - y_pts)
-        if dist < std_dvt:
+    #need better method for initial var value
+    min_x = 1000000000000
+    min_y = 1000000000000
+    max_x = None
+    max_y = None
+    matchmask_idx = 0 # bad var name
+    for xy_pts in src_pts:
+        if matchesMask[matchmask_idx] == 1:
+            #print "draw point"
+
+            #print xy_pts
+            x_pts = xy_pts[0,0]
+            y_pts = xy_pts[0,1]
             x_pts = int(x_pts)
             y_pts = int(y_pts)
+            max_x, discard_x = [max(x, 0) for x in [x_pts,max_x]]
+            min_x, discard_x = [min(x, 1000000000) for x in [x_pts,min_x]]
+            max_y, discard_y = [max(x, 0) for x in [y_pts,max_y]]
+            min_y, discard_y = [min(x, 1000000000) for x in [y_pts,min_y]]
+            #
+
+            # we got values convert into bounding box
             top_left = (x_pts + 20, y_pts + 20)
             bottom_right = (top_left[0] - 30, top_left[1] - 30)
-            cv2.rectangle(img1,top_left, bottom_right, 255, -1)
+            #cv2.rectangle(img1,top_left, bottom_right, 255, -1)
+        matchmask_idx += 1 # next value
+
+
+        #dist = math.hypot(mx_pts - x_pts, my_pts - y_pts)
+        #if dist < std_dvt:
+        #    x_pts = int(x_pts)
+        #    y_pts = int(y_pts)
+    top_left = (min_x, min_y)
+    bottom_right = (max_x, max_y)
+    cv2.rectangle(img1,top_left, bottom_right, 255, -1)
         #print x_pts
         #print y_pts
 
